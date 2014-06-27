11608b627f3917fc20266deada6651242d267b40
Paweł Szymański <out.of.logins.error@gmail.com>
2014-06-27 17:38:29 +0200

    threads

--- client.cpp
+++ client.cpp
@@ -1,23 +1,38 @@
 #include "AsioCommunication.h"
 #include "payload.hpp"
+
+#include <boost/thread/thread.hpp>
+
 #include <iostream>
+
+void Process(Payload* p)
+{
+  std::cout<<"Start"<<std::endl;
+  boost::this_thread::sleep_for( boost::chrono::seconds(2) );
+  p->Work();
+  std::cout<<"Stop"<<std::endl;
+  delete p;
+}
 
 int main()
 {
   try
-  {
-    AsioCommunication comm;
-    comm.connectAsServer();
-    Payload p=comm.readObject<Payload>();
-    std::cout<<p<<std::endl;
-    p.Work();
-    std::cout<<p<<std::endl;
-    comm.sendObject(p);
-  }
+    {
+      for(;;)
+        {
+          AsioCommunication comm;
+          comm.connectAsServer();
+          Payload* p=new Payload(comm.readObject<Payload>());
+          boost::thread worker(boost::bind(&Process, p));
+          //you could just detach the thread and throw the boost::thread object away. Doing this is usually considered bad style, unless the monitoring responsibility has been transferred to another object first. 
+          worker.detach(); 
+          comm.sendObject(*p);
+        }
+    }
   catch (std::exception& e)
-  {
-    std::cerr << e.what() << std::endl;
-  }
+    {
+      std::cerr << e.what() << std::endl;
+    }
   
   return 0;
 }
